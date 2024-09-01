b8b7ceb06d68bb81696a0d42d5ae6ddbb619e1ef
eustachekamala <eustachejared25@gmail.com>
2024-09-01 20:15:43 +0300

    Here I used then methode of a list and more other

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length <= 0:
+        print('[]')
+        return
+    
+    fibonacci_sequence = [0] if length > 0 else []
+    a, b = 0, 1
+    while len(fibonacci_sequence) < length:
+        fibonacci_sequence.append(b)
+        a, b = b, a + b
+    
+    print(fibonacci_sequence)
